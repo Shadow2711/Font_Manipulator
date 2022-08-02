ae04de5d028672407f187c97c6149c8d3e848ae9
Shadow2711 <83654982+Shadow2711@users.noreply.github.com>
2022-08-02 17:58:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+difference = "";
+leftWristX = "";
+rightWristX = "";
+function setup(){
+    canvas = createCanvas(550 , 550);
+    canvas.position(560 , 150);
+    video = createCapture(VIDEO);
+    video.size(550 , 500);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("model is loaded");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    } 
+}
+function draw(){
+    background("#1c39bb");
+    fill("#FFA500");
+    text("Ayaan" , 50 , 400);
+    textSize(difference);
+}
